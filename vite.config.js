4861f6a902e1f819ddf52794bcc0f6238c18830f
simonas <simposk@gmail.com>
2023-10-01 14:05:32 +0300

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue2';
 
 export default defineConfig({
+    server: {
+        https: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
